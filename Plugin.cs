8a319f6994022acaef96d086fa6d97036409358d
jneb802 <73610029+jneb802@users.noreply.github.com>
2023-12-13 17:51:24 -0800

    commit

---
+++ Plugin.cs
@@ -0,0 +1,41 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+
+[BepInPlugin("org.bepinex.plugins.GreydwarfAI", "Greydwarf AI", "1.0.0.0")]
+public class Plugin : BaseUnityPlugin
+{
+    private void Awake()
+    {
+        // Plugin startup logic
+        Logger.LogInfo("Greydwarf AI is loaded!");
+
+        // Apply Harmony patches
+        var harmony = new Harmony("org.bepinex.plugins.GreydwarfAI");
+        harmony.PatchAll();
+    }
+}
+
+// Ensure the namespace matches your plugin's namespace
+namespace MyFirstPlugin
+{
+    [HarmonyPatch(typeof(MonsterAI))]
+    public static class MonsterAIPatch
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch("Awake")]
+        public static void AwakePostfix(MonsterAI __instance)
+        {
+            var monsterName = __instance.gameObject.name;
+
+            // Check if the instance is one of the specified monsters
+            if (monsterName == "Greydwarf" || monsterName == "Greyling" || 
+                monsterName == "Greydwarf_Elite" || monsterName == "Greydwarf_Shaman")
+            {
+                __instance.m_circleTargetInterval = 0f;
+                __instance.m_circleTargetDuration = 0f;
+                __instance.m_circleTargetDistance = 0f;
+            }
+        }
+    }
+}
