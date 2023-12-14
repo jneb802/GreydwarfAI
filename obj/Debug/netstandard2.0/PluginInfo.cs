a98f144b052a5019594af07da98bc248d9e6b92a
jneb802 <73610029+jneb802@users.noreply.github.com>
2023-12-13 17:51:37 -0800

    commit

--- obj/Debug/netstandard2.0/PluginInfo.cs
+++ obj/Debug/netstandard2.0/PluginInfo.cs
@@ -2,8 +2,8 @@ namespace GreydwarfAI
 {
     public static class PluginInfo
     {
-        public const string PLUGIN_GUID = "GreydwarfAI";
-        public const string PLUGIN_NAME = "GreydwarfAI";
+        public const string PLUGIN_GUID = "MyFirstPlugin";
+        public const string PLUGIN_NAME = "MyFirstPlugin";
         public const string PLUGIN_VERSION = "1.0.0";
     }
 }
